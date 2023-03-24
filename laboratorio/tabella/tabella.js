a8ad238ba2c207cae1f8e1fd87dac5e5806a3f09
Simone Riva <116260360+simoriv4@users.noreply.github.com>
2023-03-24 12:02:12 +0100

    Add files via upload

---
+++ laboratorio/tabella/tabella.js
@@ -0,0 +1,10 @@
+class tabella{
+    
+    conferma()
+    {
+        document.getElementById("tabella").rows[0].innerHTML = '<img src="immagini/insalata.png" alt="insalata">';
+        document.getElementById("tabella").rows[0].innerHTML = '<img src="immagini/peperone.png" alt="peperone">';
+        document.getElementById("tabella").rows[0].innerHTML = '<img src="immagini/insalata.png" alt="insalata">';
+        document.getElementById("tabella").hidden = false;
+    }   
+}
