00e3653859b721e77033a66e959e7db1ba34881a
Simone Riva <116260360+simoriv4@users.noreply.github.com>
2023-05-04 23:11:15 +0200

    campo funzionante

--- teoria/campoMinato/campoMinato.js
+++ teoria/campoMinato/campoMinato.js
@@ -6,11 +6,12 @@ class campoMinato {
         this.RIGHE = 10;
         this.COLONNE = 10;
         this.MAXBOMBEINTORNO = 8; // massimo numero di bombe intorno ad una cella--> 8 celle        b b b
-        //                                                                b x b
-        //                                                                b b b
+        //                                                                                          b x b
+        //                                                                                          b b b
         this.MAXRIGHEBOMBE = 3;
         this.MAXCOLONNEBOMBE = 3;
         this.numBandiere = 0; // quande bandiere sono state usate
+        this.isVinto = false;
     }
 
     setNumBombe(numBombe) {
@@ -31,7 +32,7 @@ class campoMinato {
 
                 //modifico la grid in css
                 document.getElementById("grid").style.gridTemplateColumns = "auto auto auto auto auto auto auto auto auto auto";
-                document.getElementById("numBombe").innerHTML = "NUMERO BOMBE: 10";
+                document.getElementById("numBombe").innerHTML = "NUMERO BOMBE: 8";
                 break;
             case '2':
                 // difficolta 2 = campo 20x20   15 bombe
@@ -40,7 +41,7 @@ class campoMinato {
                 this.COLONNE = 20;
                 //modifico la grid in css
                 document.getElementById("grid").style.gridTemplateColumns = "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto"; // 20 colonne
-                document.getElementById("numBombe").innerHTML = "NUMERO BOMBE: 20";
+                document.getElementById("numBombe").innerHTML = "NUMERO BOMBE: 15";
                 break;
             case '3':
                 // difficolta 3 = campo 30x30   30 bombe
@@ -135,164 +136,161 @@ class campoMinato {
                 // classe bomba
                 let cell = this.getCella(randr, randc);
                 $(cell[0]).attr('id', "bomba-" + i)
-
             }
             else
                 i--;
         }
     }
 
-    //ERRATO
-    /* getBombeAdiacenti() // setto il numero di bombe adiacenti ad ogni cella
-     {
-         let nCelle = 0;
-         let righecelle = 0;
-         let colonneCelle = 0;
-         for (let r = 0; r < this.RIGHE; r++) {
-             for (let c = 0; c < this.COLONNE; c++) {
-                 let contBombe = 0; // contatore bombe intorno ad una cella
- 
-                 // se non è una bomba
-                 if (!this.campo[r][c].getBomba()) {
-                     // ciclo intorno alla cella per vedere quante bombe ci sono--> 8 celle      b b b
-                     //                                                                          b x b
-                     //                                                                          b b b
-                     // setto il numero di celle da controllare in base alla posizione
- 
-                     if ((r !== 0 && c === 0 && r !== this.RIGHE)) {
-                         nCelle = 6; // 5 celle da controllare +1 (cella cliccata non da controllare)
-                         righecelle = 3;
-                         colonneCelle = 2;
-                     }
-                     else if(r === 0 && c !== 0 && c !== this.COLONNE)
-                     {
-                         nCelle = 6; // 5 celle da controllare +1 (cella cliccata non da controllare)
-                         righecelle = 2;
-                         colonneCelle = 3;
-                     }
-                     else if ((r === 0 && c === 0) || (r === 0 && c === this.COLONNE) || (c === 0 && r === this.RIGHE) || (r === this.RIGHE && c === this.COLONNE)) {
-                         nCelle = 4; // 3 celle da controllare +1 (cella cliccata non da controllare)
-                         righecelle = 2;
-                         colonneCelle = 2;
-                     }
-                     else {
-                         nCelle = this.MAXBOMBEINTORNO + 1; // numero di bombe massimo (8) +1 (cella cliccata non da controllare)
-                         righecelle = 3;
-                         colonneCelle = 3;
-                     }
- 
- 
-                     let rtemp = 0;
-                     let ctemp = 0;
-                     if (r > 0)
-                         rtemp = r - 1; // inizia dalla riga precedente
-                     if (c > 0)
-                         ctemp = c - 1; // inizia dalla colonna precedente
- 
-                     for (let r2 = rtemp; r2 < righecelle; r2++) {
-                         for (let c2 = ctemp; c2 < colonneCelle; c2++) {
-                             if (this.campo[r2][c2].getBomba() && c !== ctemp && r !== rtemp) // salto la cella cliccata
-                                 contBombe++;
-                         }
-                     }
-                     this.campo[r][c].setNumBombe(contBombe);
-                 }
-             }
-         }
-     }*/
-
-    setBombeAdiacenti() {// setto il numero di bombe adiacenti ad ogni cella
+    setBombeAdiacenti() { // setto il numero di bombe adiacenti ad ogni cella
         for (let r = 0; r < this.RIGHE; r++) {
             for (let c = 0; c < this.COLONNE; c++) {
-                let contBombe = 0;
-                if (!this.campo[r][c].getBomba()) { // controllo che la cella non sia una bomba--> non deve contenere numeri
-                    // cicli intorno alla cella da controllare
-                    for (let r2 = Math.max(0, r - 1); r2 <= Math.min(r + 1, this.RIGHE - 1); r2++) {// utilizzando Math.max e Math.min evito di uscire dalla matrice--> parto da 0 continuerò con r-1
-                        for (let c2 = Math.max(0, c - 1); c2 <= Math.min(c + 1, this.COLONNE - 1); c2++) {
+                let numBombeAdiacenti = 0;
+    
+                // controlla le celle adiacenti --> max 8
+                for (let r2 = r - 1; r2 <= r + 1; r2++) {
+                    for (let c2 = c - 1; c2 <= c + 1; c2++) {
+                        // controllo che la cella adiacente sia all'interno del campo
+                        if (r2 >= 0 && r2 < this.RIGHE && c2 >= 0 && c2 < this.COLONNE) {
                             if (this.campo[r2][c2].getBomba()) {
-                                contBombe++;
+                                numBombeAdiacenti++;
                             }
                         }
                     }
-                    this.campo[r][c].setNumBombe(contBombe);
                 }
+    
+                // imposto il numero di bombe adiacenti alla cella corrente
+                this.campo[r][c].setNumBombe(numBombeAdiacenti);
             }
         }
     }
 
+    
+
     clickCella(cella) {
-        // ricavo riga e colonna
-        //trovo la riga della cella
-        let riga = cella.getAttribute("data-row");
+        if(!this.isVinto) // se non si ha vinto si può cliccare, altrimenti non succede nulla
+        {
+            // ricavo riga e colonna
+            //trovo la riga della cella
+            let riga = cella.getAttribute("data-row");
 
-        //trovo la colonna della cella
-        let colonna = cella.getAttribute("data-col");
+            //trovo la colonna della cella
+            let colonna = cella.getAttribute("data-col");
 
-        // se non è un flag bandiera
-        if (cella.getAttribute("class") !== "cellaBandiera") {
-            if (this.campo[riga][colonna].getBomba()) {
-                // rivelo tutte le bombe
-                // cambio la classe della div da cellaCoperta in cellaBomba
-                $(cella).removeClass("cellaCoperta");
-                $(cella).addClass("cellaBomba");
+            // se non è un flag bandiera
+            if (cella.getAttribute("class") !== "cellaBandiera") {
+                if (this.campo[riga][colonna].getBomba()) {
+                    // rivelo tutte le bombe
+                    // cambio la classe della div da cellaCoperta in cellaBomba
+                    $(cella).removeClass("cellaCoperta");
+                    $(cella).addClass("cellaBomba");
 
-                for (let r = 0; r < this.RIGHE; r++) {
-                    for (let c = 0; c < this.COLONNE; c++) {
-                        if (this.campo[r][c].getBomba()) {
-                            // cambio il contenuto del div risultato
-                            $("#risultato").text("GAME OVER");
+                    for (let r = 0; r < this.RIGHE; r++) {
+                        for (let c = 0; c < this.COLONNE; c++) {
+                            if (this.campo[r][c].getBomba()) {
+                                // cambio il contenuto del div risultato
+                                $("#risultato").text("GAME OVER");
 
-                            // cella corrente
-                            let tmp = this.getCella(r, c);
-                            $(tmp[0]).removeClass("cellaCoperta");
-                            $(tmp[0]).addClass("cellaBomba");
+                                // cella corrente
+                                let tmp = this.getCella(r, c);
+                                $(tmp[0]).removeClass("cellaCoperta");
+                                $(tmp[0]).addClass("cellaBomba");
+                            }
+                        }
+                    }
+                    $(document).ready(function () {
+                        $("#risultato").slideDown("slow");
+                    });
+                }
+                else if (this.campo[riga][colonna].getNumBombe() !== 0) {
+                    //alert(this.campo[riga][colonna].getNumBombe());
+                    // cambio su cella numero
+                    let cella = this.getCella(riga, colonna);
+                    $(cella[0]).removeClass("cellaCoperta");
+                    $(cella[0]).addClass("cellaNumero");
+                    // mostro il numero 
+                    let tmp = this.getCella(riga, colonna);
+                    $(tmp[0]).text(this.campo[riga][colonna].getNumBombe());
+                }
+                else if (this.campo[riga][colonna].getNumBombe() === 0) {
+                    // faccio apparire tutte le celle vuote contenute nel "muro"
+                    this.apriCelleVuoteAdiacenti(riga, colonna);
+                }
+            }
+            this.controllaVincita();
+        }
+
+    }
+
+    apriCelleVuoteAdiacenti(riga, colonna) {
+        // apro la cella corrente
+        let cella = this.getCella(riga, colonna);
+        $(cella[0]).removeClass("cellaCoperta");
+        $(cella[0]).addClass("cellaAperta");
+
+    
+        // controlla le celle adiacenti --> max 8
+        for (let r = riga - 1; r <= riga + 1; r++) {
+            for (let c = colonna - 1; c <= colonna + 1; c++) {
+                // Ccntrollo che la cella sia all'interno del campo
+                if (r >= 0 && r < this.RIGHE && c >= 0 && c < this.COLONNE) {
+                    let cellaAdiacente = this.getCella(r, c);
+    
+                    // Rivela la cella adiacente se non è già stata rivela o è una bomba
+                    if (cellaAdiacente[0].getAttribute("class") === "cellaCoperta" && !this.campo[r][c].getBomba()) {
+
+                        if (this.campo[r][c].getNumBombe() === 0){ // se la cella non è adiacente ad una bomba allora richiama il metodo per rivelare la successiva
+                            $(cellaAdiacente[0]).removeClass("cellaCoperta");
+                            $(cellaAdiacente[0]).addClass("cellaAperta");
+                            this.apriCelleVuoteAdiacenti(r, c);
+                        }
+                        else
+                        {
+                            // se è adiacente ad una bomba cambio la classe della cella e scrivo il numero di bombe
+                            $(cellaAdiacente[0]).removeClass("cellaCoperta");
+                            $(cellaAdiacente[0]).addClass("cellaNumero");
+                            $(cellaAdiacente[0]).text(this.campo[r][c].getNumBombe());
+
                         }
                     }
                 }
-                $(document).ready(function () {
-                    $("#risultato").slideDown("slow");
-                });
-            }
-            else if (this.campo[riga][colonna].getNumBombe() !== 0) {
-                //alert(this.campo[riga][colonna].getNumBombe());
-
-                // mostro il numero 
-                let tmp = this.getCella(riga, colonna);
-                $(tmp[0]).text(this.campo[riga][colonna].getNumBombe());
-            }
-            else if (this.campo[riga][colonna].getNumBombe() === 0) {
-                // faccio apparire tutte le celle vuote contenute nel "muro"
-                this.celleAperte(riga, colonna);
             }
         }
-
     }
 
-    celleAperte(r, c) {
-        if (!this.campo[r][c].getRivelato()) {
-            // Rivelo la cella corrente
-            let cella = this.getCella(r, c);
-            $(cella[0]).removeClass("cellaCoperta");
-            $(cella[0]).addClass("cellaAperta");
-            this.campo[r][c].setRivelato(true);
-            // Scorro le celle adiacenti
-
-            // controlla le celle sopra alla corrente
-            // appena incontra una cella con un numero(adiacente ad una o più bombe) cambia direzione --> destra
-            // appena ne incotntra un'altra cambia verso il basso
-            // appena ne incotra un'altra cambia verso sinistra
-            // controlla che
-            
-        }
-    }
     getCella(r, c) {
         // ritorna un vettore con un solo elemento-> la cella che si trova alle coordinate specificate
         return $("div[data-row='" + r + "'][data-col='" + c + "']");
     }
 
+    controllaVincita()
+    {
+        let celleScoperte = 0;
+        for(let r = 0; r < this.RIGHE; r++)
+        {
+            for(let c= 0; c<this.COLONNE; c++)
+            {
+                let tmp = this.getCella(r, c);
+                let classe = tmp[0].getAttribute("class");
+                if(!this.campo[r][c].getBomba() && (classe !== "cellaCoperta" && classe !== "cellaBandiera"))
+                {
+                    celleScoperte++;
+                }
+            }
+        }
+        if(((this.RIGHE*this.COLONNE)-celleScoperte) === this.numBombe) // deve essere uguale al     totale delle celle - numero delle bombe
+        {
+            $("#risultato").text("VITTORIA!");
+            $(document).ready(function(){
+                $("#risultato").slideDown("slow");
+            });
+            this.isVinto = true; // imposto la booleana su true in modo da non poter cliccare più nulla
+        }
+    }
     reset() {
         this.setDifficolta(); // resetto la partita
         this.numBandiere = 0;
+        this.isVinto = false;
     }
 
 
