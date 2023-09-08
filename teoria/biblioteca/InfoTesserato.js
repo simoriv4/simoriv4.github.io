b12977c73c430261fdece760c556e3bf0c9c06ce
simoriv4 <simone@riva.co.it>
2023-09-08 18:07:54 +0200

    aggiunta catalogo libri  e il noleggio(correggere la data)

--- teoria/biblioteca/InfoTesserato.js
+++ teoria/biblioteca/InfoTesserato.js
@@ -17,17 +17,35 @@ class InfoTesserato{
         let tesserato = this.array[this.getParametro()];
         let libriNoleggiati = tesserato.libriNoleggiati;
 
-        let informazioni = $("<li> Data di Nascita: "+ tesserato.dataNascita +"</li><li>Luogo di Nascita: "+ tesserato.luogoDiNascita+"</li>"+"<li> Residenza: " + tesserato.residenza+"</li>"+"<li>Domicilio: " + tesserato.domicilio +"</li>");
+        let informazioni = $(`<li> Data di Nascita: ${tesserato.dataNascita}</li><li>Luogo di Nascita: ${tesserato.luogoDiNascita}</li><li> Residenza: ${tesserato.residenza}</li><li>Domicilio: ${tesserato.domicilio}</li>`);
         let libri = "";
         for(let i = 0; i < libriNoleggiati.lentgth; i++)
         {
             libri+="<li>" + libriNoleggiati[i]+"</i>";
         }
+        $("#nomeTesserato").html(tesserato.nome + " " + tesserato.cognome);
         $("#Informazioni").append(informazioni);
         $("#LibriInNoleggio").append(libri);
 
 
     }
+
+    rimuoviTesserato()
+    {
+        this.array.splice(this.getParametro(), 1);
+
+        this.salvaSuFile();
+            window.location.href = `index.html?parametro=${true}`;
+    }
+
+    salvaSuFile() {
+        this.clearFile();
+        //salvo in una stringa gli elementi
+        for (let i = 0; i < this.array.length; i++) {
+            localStorage.setItem('Tesserato' + (i + 1), this.array[i].visualizza());
+        }
+    }
+
 
     salvaDaFile() {
         this.array = [];
@@ -37,7 +55,7 @@ class InfoTesserato{
         while (tmp != undefined) {
             tmp = localStorage.getItem('Tesserato' + (i + 1));
             let vett = tmp.split(';');
-            let temp = new Tesserato(vett[0], vett[1], vett[2], vett[3]);
+            let temp = new Tesserato(vett[0], vett[1], vett[2], vett[3], vett[4], vett[5], vett[6], vett[7]);
             this.array[i] = temp;
             i++;
             tmp = localStorage.getItem('Tesserato' + (i + 1));
@@ -47,6 +65,9 @@ class InfoTesserato{
 
     }
 
+    clearFile() {
+        localStorage.clear();
+    }
 
 
 
--- teoria/biblioteca/Libro.js
+++ teoria/biblioteca/Libro.js
@@ -1,27 +1,35 @@
-class Libro{
+class Libro {
 
     // costruttore parametrico
-    constructor(titolo, dataNoleggio, iscomplete)
-    {
+    constructor(titolo, autore,casaEditrice, annoPubblicazione, IsNoleggiato, dataNoleggio, immagine ) {
         this.titolo = titolo;
+        this.autore = autore;
         this.dataNoleggio = dataNoleggio; // formato data da input--> aaaa/mm/gg
-        this.dataConsegna = generateDate(dataNoleggio);
+        // this.dataScadenzaNoleggio = "";
+        this.dataScadenzaNoleggio = this.generaData(this.dataNoleggio);
+        this.IsNoleggiato = IsNoleggiato;
+        this.casaEditrice = casaEditrice;
+        this.annoDiPubblicazione = annoPubblicazione;
+        this.immagine = immagine;
     }
 
-    generateDate(dataNoleggio)
-    {
+    generaData(dataNoleggio) {
         let data = new Date(dataNoleggio);
-       // data.setDate(data.getDate() +  30)
+        data.setDate(data.getDate() +  30);
+        let anno = data.getFullYear();
+        let mese = (data.getMonth() + 1).toString().padStart(2, '0');
+        let giorno = data.getDate().toString().padStart(2, '0'); 
+        return `${anno}/${mese}/${giorno}`;
+      
+        return data;
     }
-    posticipaConsegna()
-    {
+    posticipaConsegna() {
         // posticipa di un mese la consegna
-//        this.dataNoleggio
+        //        this.dataNoleggio
     }
 
-    visualizza()
-    {
-        return this.titolo+";"+this.dataNoleggio + ";" + this.IsComplete;
+    visualizza() {
+        return `${this.titolo};${this.dataNoleggio};${this.dataConsegna};${this.IsNoleggiato}`;
     }
-    
+
 }
